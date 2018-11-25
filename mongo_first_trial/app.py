66f8aef638cdae581b6510321429422525cd8bf6
Nidhi Patel <nidhipatel114@yahoo.com>
2018-11-25 13:58:32 -0500

    Displaying Transactions
    - used MLAB that provides Database-as-a-service feature.

---
+++ mongo_first_trial/app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,jsonify,json,request
+import json
+import pymongo
+import bson;
+from flask_pymongo import PyMongo
+from bson import json_util
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'mongo_first_db'
+app.config['MONGO_URI'] = 'mongodb://nidhi1:nnp93.66@ds119350.mlab.com:19350/mongo_first_db'
+
+mongo = PyMongo(app)
+
+@app.route('/items', methods=['GET'])
+def get_all_items():
+	items = mongo.db.transactions
+	
+	output = []
+	for each in items.find():
+		output.append(each)
+	return jsonify({output})
+
+def add():
+	user = mongo.db.users
+	user.insert()
+	return 'Added user!'
+
+if __name__ == '__main__':
+	app.run(debug=True)
