b6be357bed22427f5958ba4386733798888ad3ea
Nidhi Patel <nidhipatel114@yahoo.com>
2018-11-25 14:23:24 -0500

    Changed Username and Password

--- mongo_first_trial/app.py
+++ mongo_first_trial/app.py
@@ -8,14 +8,14 @@ from bson import json_util
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'mongo_first_db'
-app.config['MONGO_URI'] = 'mongodb://nidhi1:nnp93.66@ds119350.mlab.com:19350/mongo_first_db'
+app.config['MONGO_URI'] = 'mongodb://nidhi:nidhi123@ds119350.mlab.com:19350/mongo_first_db'
 
 mongo = PyMongo(app)
 
 @app.route('/items', methods=['GET'])
 def get_all_items():
 	items = mongo.db.transactions
-	
+
 	output = []
 	for each in items.find():
 		output.append(each)
