d7e8bb51a780d65605d62c378ec2925592f98148
Nidhi Patel <nidhipatel114@yahoo.com>
2018-12-15 18:33:46 -0500

    Update app.py
    - issue is resolved

--- mongo_first_trial/app.py
+++ mongo_first_trial/app.py
@@ -1,30 +1,50 @@
 from flask import Flask,render_template,jsonify,json,request
 import json
 import pymongo
-import bson;
 from flask_pymongo import PyMongo
-from bson import json_util
+from bson import ObjectId
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'mongo_first_db'
-app.config['MONGO_URI'] = 'mongodb://nidhi:nidhi123@ds119350.mlab.com:19350/mongo_first_db'
+auth = HTTPBasicAuth()
+uri = 'mongodb://nidhi:nidhi123@ds119350.mlab.com:19350/mongo_first_db' 
+client = pymongo.MongoClient(uri)
+db = client['mongo_first_db']
+items = db['transactions']
 
-mongo = PyMongo(app)
+@auth.get_password
+def get_password(username):
+	if username == 'nidhi':
+		return 'nidhi'
+	return None
 
-@app.route('/items', methods=['GET'])
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+@app.route('/search', methods=['GET'])
 def get_all_items():
-	items = mongo.db.transactions
+	docs = [doc for doc in db.items.find()]
+	print(docs)
+	JSONEncoder().encode(docs)
+	return jsonify({'docs':JSONEncoder().encode(docs)})
+	
+@app.route('/search/<item>', methods=['GET'])
+def get_one_item(item):
+	docs = [doc for doc in db.items.find({'item':item})]
+	#print(docs)
+	JSONEncoder().encode(docs)
+	return jsonify({'docs':JSONEncoder().encode(docs)})
 
-	#output = []
-	#for each in items.find():
-	#	output.append(each)
-	return jsonify({items})
-
+@app.route('/add', methods=['GET'])
 def add():
 	user = mongo.db.users
 	user.insert()
 	return 'Added user!'
 
-if __name__ == '__main__':
-	app.run(debug=True)
+if __name__ =="__main__":
+	app.run(debug = True)
